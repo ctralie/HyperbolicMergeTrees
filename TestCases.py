8b9abf4fc75e922da24885ea3c189cae6932de03
Chris Tralie <chris.tralie@gmail.com>
2018-08-24 17:51:23 -0400

    Generalizing code for polynomial systems

--- TestCases.py
+++ TestCases.py
@@ -2,30 +2,14 @@ import scipy.optimize as opt
 import numpy as np
 import matplotlib.pyplot as plt
 from HypMergeTree import *
+from PolynomialSystem import g, gradg
 
-def f1(x, ws):
-    x1, x2 = x
-    w1, w2, w3, w4, w5, w6, w7 = ws
-    return w1*w5-w6*w2-w2**2 + x1*(w1+w5+2*w2+w6) - (w2+w6)*x2 + x1*x2
-
-def f2(x, ws):
-    x1, x2 = x
-    w1, w2, w3, w4, w5, w6, w7 = ws
-    return w6*w2-w3*w7-w4*w7-w3**2-w3*w4 - w6*x1 + (w6+w7+2*w3+w4+w2)*x2 - x1*x2
-
-def g(x, ws):
-    return 0.5*(f1(x, ws)**2 + f2(x, ws)**2)
-
-def gradg(x, ws):
-    x1, x2 = x
-    w1, w2, w3, w4, w5, w6, w7 = ws
-    g1 = f1(x, ws)*(w1+w5+2*w2+w6+x2) - f2(x, ws)*(w6+x2)
-    g2 = f1(x, ws)*(x1-w2-w6) + f2(x, ws)*(w6+w7+2*w3+w2+w4-x1)
-    return np.array([g1, g2])
-
-def get_pentagon_xs(ws, verbose = True):
-    x0 = np.ones(2)
-    res = opt.minimize(g, x0, args = (ws), method='BFGS', jac=gradg)
+def get_pentagon_xs(ws, x0, verbose = False):
+    # a = w5, b = w1, c = w2, d = w6, e = x2
+    idxs1 = [0, 6, 2, 3, 7, 1]
+    # a = w6, b = (w2-x1), c = w3, d = w7, e = w4
+    idxs2 = [1, 7, [0, 3], 4, 8, 5]
+    res = opt.minimize(g, x0, args = (ws, [idxs1, idxs2]), method='BFGS', jac=gradg)
     xsol = res['x']
     if verbose:
         print("g(x0) = ", g(x0, ws))
@@ -33,8 +17,8 @@ def get_pentagon_xs(ws, verbose = True):
         print("g(xsol) = ", g(xsol, ws))
     return xsol
 
-def get_pentagon_zs_rs(ws):
-    xs = get_pentagon_xs(ws)
+def get_pentagon_zs_rs(ws, x0 = np.ones(2)):
+    xs = get_pentagon_xs(ws, x0)
     z1 = 1.0
     getz2 = lambda z1, xs, ws: z1*(xs[0]+ws[4])/(ws[1]-xs[0]+ws[5])
     getz3 = lambda z2, xs, ws: z2*(xs[1]+ws[5])/(ws[2]-xs[1]+ws[6])
@@ -51,18 +35,11 @@ def get_pentagon_zs_rs(ws):
 
     return xs, np.array([0, z1, z1+z2, z1+z2+z3]), np.array([r0, r1, r2, r3, rinf])
 
-
-def neg_weight_case():
+def pentagon_edgecollapse():
     np.random.seed(3)
     ws = np.random.rand(7)
     ws = np.round(ws*10)/10.0
-
-    """    
     print("ws = ", ws)
-    print("zs = ", zs)
-    print("rs = ", rs)
-    """
-
     HMT = HypMergeTree()
     rscale = 0.5
 
@@ -85,6 +62,5 @@ def neg_weight_case():
         plt.title("x1 = %.3g, x2 = %.3g"%tuple(xs.tolist()))
         plt.savefig("%i.png"%i)
 
-
 if __name__ == '__main__':
-    neg_weight_case()
+    pentagon_edgecollapse()
